08e57720c2889b568b88777f383281788ec052d8
backstab319 <dev.siddhartha.gupta@gmail.com>
2020-01-08 12:40:14 +0530

    Initial baseplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './subscribe/contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const myRoutes: Routes = [
+  {path: '', component: NewsComponent, pathMatch: 'full'},
+  {path: 'news', component: NewsComponent, pathMatch: 'full'},
+  {path: 'contact', component: ContactComponent, pathMatch: 'full'},
+  {path: 'about', component: AboutComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(myRoutes),
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
